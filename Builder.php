98f2e5583f46f2dcd5bd87793825e341a0f2e0e8
Calin Rada <crada@solomo365.com>
2013-11-19 21:59:35 +0100

    A first try to build

--- Builder.php
+++ Builder.php
@@ -7,6 +7,7 @@ namespace Crada\Apidoc;
 use Crada\Apidoc\Extractor;
 use Crada\Apidoc\View;
 use Crada\Apidoc\View\JsonView;
+use Crada\Apidoc\Exception;
 
 /**
  * Crada\Apidoc\Builder
@@ -48,9 +49,95 @@ class Builder
         return $st_output[1];
     }
 
-    private function buildTemplate()
+    private function saveTemplate($data)
+    {
+        $template   = __DIR__.'/Resources/views/template/index.html';
+        $outputDir  = __DIR__.'/Resources/views/docs';
+        $oldContent = file_get_contents($template);
+        $newContent = str_replace("##content##", $data, $oldContent);
+
+        if(!is_dir($outputDir)) {
+            if(!mkdir($outputDir)){
+                throw new Exception("I can't create directory");
+            }
+        }
+
+        if(!file_put_contents($outputDir.'/index.html', $newContent)) {
+            throw new Exception("I can't save the content to $outputDir");
+        }
+    }
+
+    private function generateTemplate()
     {
         $st_annotations = $this->extractAnnotations();
+
+        $template = '';
+
+        foreach($st_annotations as $class => $methods)
+        {
+            foreach($methods as $name => $docs) {
+                if(count($docs) == 0) continue;
+                $template .= '
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion"
+                                    href="#collapseOne">'.strtoupper($docs['ApiMethod'][0]['type']).' '.$name.' </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <!-- Nav tabs -->
+                                <ul class="nav nav-tabs" id="php-apidoctab">
+                                  <li class="active"><a href="#info" data-toggle="tab">Info</a></li>
+                                  <li><a href="#sandbox" data-toggle="tab">Sandbox</a></li>
+                                </ul>
+
+                                <!-- Tab panes -->
+                                <div class="tab-content">
+                                  <div class="tab-pane active" id="info">
+                                       '.$docs['ApiDescription'][0]['description'].'
+                                        <hr>
+                                        '.$this->generateParamsTemplate($docs).'
+
+                                  </div>
+                                  <div class="tab-pane" id="sandbox">
+                                      <p>Soon...</p>
+                                  </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+            }
+        }
+
+        $this->saveTemplate($template);
+
+        return true;
+    }
+
+    private function generateParamsTemplate($st_params)
+    {
+        if(!isset($st_params['ApiParams'])) {
+            return;
+        }
+
+        $header = '<table class="table table-hover"><thead><tr><th>Name</th>
+         <th>Type</th><th>Required</th><th>Description</th></tr></thead>
+         <tbody>';
+
+        $body = '';
+
+        foreach($st_params['ApiParams'] as $params) {
+            $body .= '<tr><td>'.$params['name'].'</td>';
+            $body .= '<td>'.$params['type'].'</td>';
+            $body .= '<td>'.$params['nullable'].'</td>';
+            $body .= '<td>'.$params['description'].'</td></tr>';
+        }
+
+        $footer = '</tbody></table>';
+
+        return $header.$body.$footer;
     }
 
     /**
@@ -82,6 +169,6 @@ class Builder
      */
     public function generate()
     {
-        return $this->buildTemplate();
+        return $this->generateTemplate();
     }
 }
