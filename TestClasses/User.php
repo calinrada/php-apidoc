9324a4c58a2e0c8355f9ee54509dde72a54146f2
Calin Rada <crada@solomo365.com>
2013-11-20 14:27:31 +0100

    Added ApiRoute annotation to test classes

--- TestClasses/User.php
+++ TestClasses/User.php
@@ -5,7 +5,8 @@ class User
 {
     /**
      * @ApiDescription(section="User", description="Get information about user")
-     * @ApiMethod(type="get", route="/user/get/{id}")
+     * @ApiMethod(type="get")
+     * @ApiRoute(name="/user/get/{id}")
      * @ApiParams(name="id", type="integer", nullable=false, description="User id")
      */
     public function get()
@@ -16,6 +17,7 @@ class User
     /**
      * @ApiDescription(section="User", description="Create's a new user")
      * @ApiMethod(type="post", route="/user/create")
+     * @ApiRoute(name="/user/create")
      * @ApiParams(name="username", type="string", nullable=false, description="Username")
      * @ApiParams(name="email", type="string", nullable=false, description="Email")
      * @ApiParams(name="password", type="string", nullable=false, description="Password")
@@ -28,7 +30,8 @@ class User
 
     /**
      * @ApiDescription(section="User", description="Delete a user")
-     * @ApiMethod(type="delete", route="/user/delete")
+     * @ApiMethod(type="delete")
+     * @ApiRoute(name="/user/delete")
      * @ApiParams(name="id", type="integer", nullable=false, description="User id")
      */
     public function delete()
@@ -38,7 +41,8 @@ class User
 
     /**
      * @ApiDescription(section="User", description="Delete a user")
-     * @ApiMethod(type="put", route="/user/update")
+     * @ApiMethod(type="put")
+     * @ApiRoute(name="/user/update")
      * @ApiParams(name="id", type="integer", nullable=false, description="User id")
      */
     public function update()
